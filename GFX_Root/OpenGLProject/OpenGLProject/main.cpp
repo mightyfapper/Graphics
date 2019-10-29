a6db15ea617c7be08791c0eedd115b8d7b3b4c18
mightyfapper <oojackmaruoo@gmail.com>
2019-10-29 12:11:56 +0200

    Changed background color
    Tehe

--- GFX_Root/OpenGLProject/OpenGLProject/main.cpp
+++ GFX_Root/OpenGLProject/OpenGLProject/main.cpp
@@ -59,7 +59,7 @@ int main()
 
 		// Render
 		// Clear the colorbuffer
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		// Swap the screen buffers
